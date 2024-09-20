8bd8b9d1c28b5ac120173752a3270672a30eb88b
ayush-17-05 <aayushudhani17@gmail.com>
2024-09-20 11:00:07 +0530

    hello mc

---
+++ minithon/app/event/page.tsx
@@ -0,0 +1,12 @@
+import EventsPage from "@/components/EventsPage";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <EventsPage />
+    </div>
+  );
+};
+
+export default page;
